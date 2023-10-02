4f07bec9fe8ee73eb0b8917dbe5d4083f7c8b157
snowsakurasky <cc30428022@gmail.com>
2023-10-02 19:26:22 +0800

    modify main.py allow user input number

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 1002
- the scripting  language 10/2 
+ the scripting  language 10/2
+
+Demo basic skill
---
+++ main.py
@@ -0,0 +1,6 @@
+def addnum(a, b):
+    return a+b
+
+x = int(input("Please input number1:"))
+y = int(input("Please input number2:"))
+print(addnum(x, y))
